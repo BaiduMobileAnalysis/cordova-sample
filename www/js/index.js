4e9f3913b293e5884e8a17346b2b99e82463ee31
lidongdong01 <lidongdong01@baidu.com>
2018-06-28 15:57:10 +0800

    update demo

--- www/js/index.js
+++ www/js/index.js
@@ -18,103 +18,103 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-        
+initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+},
+    
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.init();
-    },
+onDeviceReady: function() {
+    this.init();
+},
     
-    init: function(){
+init: function(){
     
-        // 事件一
-        var event1Button = document.getElementById('event1');
-        event1Button.addEventListener('click', this.onEvent1Click.bind(this), false);
-        
-        
-        // 事件二
-        var event2Button = document.getElementById('event2');
-        event2Button.addEventListener('click', this.onEvent2Click.bind(this), false);
-        
-        
-        // 事件三
-        var event3StartButton = document.getElementById('event3Start');
-        event3StartButton.addEventListener('click', this.onEvent3StartClick.bind(this), false);
-        
-        var event3EndButton = document.getElementById('event3End');
-        event3EndButton.addEventListener('click', this.onEvent3EndClick.bind(this), false);
-
-        // 事件四
-        var event4Button = document.getElementById('event4');
-        event4Button.addEventListener('click', this.onEvent4Click.bind(this), false);
-        // 事件五
-        var event5Button = document.getElementById('event5');
-        event5Button.addEventListener('click', this.onEvent5Click.bind(this), false);
-        
-        //  事件六
-        var event6StartButton = document.getElementById('event6Start');
-        event6StartButton.addEventListener('click', this.onEvent6StartClick.bind(this), false);
-        
-        var event6EndButton = document.getElementById('event6End');
-        event6EndButton.addEventListener('click', this.onEvent6EndClick.bind(this), false);
-
-        // 页面
-        var pageStartButton = document.getElementById('pageStart');
-        pageStartButton.addEventListener('click', this.onPageStart.bind(this), false);
-        
-        var pageEndButton = document.getElementById('pageEnd');
-        pageEndButton.addEventListener('click', this.onPageEnd.bind(this), false);
-        
-    },
-
+    // 事件一
+    var event1Button = document.getElementById('event1');
+    event1Button.addEventListener('click', this.onEvent1Click.bind(this), false);
+    
+    
+    // 事件二
+    var event2Button = document.getElementById('event2');
+    event2Button.addEventListener('click', this.onEvent2Click.bind(this), false);
+    
+    
+    // 事件三
+    var event3StartButton = document.getElementById('event3Start');
+    event3StartButton.addEventListener('click', this.onEvent3StartClick.bind(this), false);
+    
+    var event3EndButton = document.getElementById('event3End');
+    event3EndButton.addEventListener('click', this.onEvent3EndClick.bind(this), false);
+    
+    // 事件四
+    var event4Button = document.getElementById('event4');
+    event4Button.addEventListener('click', this.onEvent4Click.bind(this), false);
+    // 事件五
+    var event5Button = document.getElementById('event5');
+    event5Button.addEventListener('click', this.onEvent5Click.bind(this), false);
+    
+    //  事件六
+    var event6StartButton = document.getElementById('event6Start');
+    event6StartButton.addEventListener('click', this.onEvent6StartClick.bind(this), false);
+    
+    var event6EndButton = document.getElementById('event6End');
+    event6EndButton.addEventListener('click', this.onEvent6EndClick.bind(this), false);
+    
+    // 页面
+    var pageStartButton = document.getElementById('pageStart');
+    pageStartButton.addEventListener('click', this.onPageStart.bind(this), false);
+    
+    var pageEndButton = document.getElementById('pageEnd');
+    pageEndButton.addEventListener('click', this.onPageEnd.bind(this), false);
+    
+},
+    
     
     // 百度移动统计DEMO代码
-    onEvent1Click: function() {
-        BaiduMobStat.onEvent('event1', '事件一');
-    },
-
-    onEvent2Click: function() {
-        BaiduMobStat.onEventDuration('event2', '事件二', 1000);
-    },
+onEvent1Click: function() {
+    BaiduMobStat.onEvent('event1', '事件一');
+},
+    
+onEvent2Click: function() {
+    BaiduMobStat.onEventDuration('event2', '事件二', 1000);
+},
     
     
-    onEvent3StartClick: function() {
-        BaiduMobStat.onEventStart('event3', '事件三');
-    },
+onEvent3StartClick: function() {
+    BaiduMobStat.onEventStart('event3', '事件三');
+},
     
-    onEvent3EndClick: function() {
-        BaiduMobStat.onEventEnd('event3', '事件三');
-    },
+onEvent3EndClick: function() {
+    BaiduMobStat.onEventEnd('event3', '事件三');
+},
     
     
-    onEvent4Click:function() {
-        BaiduMobStat.onEventWithAttributes('event4', '事件四', {'分类':'分类一'});
-    },
-
-    onEvent5Click:function() {
-        BaiduMobStat.onEventDurationWithAttributes('event5', '事件五', 1000, {'分类':'分类一'});
-    },
+onEvent4Click:function() {
+    BaiduMobStat.onEventWithAttributes('event4', '事件四', {'分类':'分类一'});
+},
     
-    onEvent6StartClick: function() {
-        BaiduMobStat.onEventStart('event6', '事件六');
-    },
+onEvent5Click:function() {
+    BaiduMobStat.onEventDurationWithAttributes('event5', '事件五', 1000, {'分类':'分类一'});
+},
     
-    onEvent6EndClick: function() {
-        BaiduMobStat.onEventEndWithAttributes('event6', '事件六', {'分类':'分类一'});
-    },
+onEvent6StartClick: function() {
+    BaiduMobStat.onEventStart('event6', '事件六');
+},
     
-    onPageStart: function() {
-        BaiduMobStat.onPageStart('页面一');
-    },
+onEvent6EndClick: function() {
+    BaiduMobStat.onEventEndWithAttributes('event6', '事件六', {'分类':'分类一'});
+},
     
-    onPageEnd: function() {
-        BaiduMobStat.onPageEnd('页面一');
-    },
+onPageStart: function() {
+    BaiduMobStat.onPageStart('页面一');
+},
+    
+onPageEnd: function() {
+    BaiduMobStat.onPageEnd('页面一');
+},
     
 };
 
